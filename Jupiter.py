50140b0e5fc305a72473b828226c24c651f5643a
YassauiMaksut <105439934+YassauiMaksut@users.noreply.github.com>
2022-05-13 00:06:29 +0600

    Create Jupiter.py

---
+++ Jupiter.py
@@ -0,0 +1,5 @@
+# My Jupyter Notebook on IBM Watson Studio
+**Yassaui Maksut I'm only Human after all**
+*I am interested in data science because it is interesting*
+### It adds two numbers
+print(1+1)
