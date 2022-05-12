fe7ffb44d463f839baad1328cddeeb0e9d4710f2
YassauiMaksut <105439934+YassauiMaksut@users.noreply.github.com>
2022-05-13 00:09:07 +0600

    Update Jupiter.py

--- Jupiter.py
+++ Jupiter.py
@@ -3,3 +3,9 @@
 *I am interested in data science because it is interesting*
 ### It adds two numbers
 print(1+1)
+---
+> Blockquotes are very handy in email to emulate reply text.
+Markdown | Less | Pretty
+--- | --- | ---
+*Still* | `renders` | **nicely**
+1 | 2 | 3
